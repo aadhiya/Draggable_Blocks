48bd9769d14c2588cc52198871dd5d243ec917d1
aadhiya <aadhiyamaria@gmail.com>
2025-01-27 08:25:08 +0300

    Implement draggable text block with edit and save functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Draggable Text Box</h1>
+
+    <!-- Draggable Component -->
+    <VueDraggable v-model="blocks" :group="{ name: 'blocks' }">
+      <div v-for="(block, index) in blocks" :key="index" class="block">
+        <textarea v-if="block.type === 'text'" v-model="block.text" class="block-text"></textarea>
+        <button @click="removeBlock(index)">Remove</button>
+      </div>
+    </VueDraggable>
+
+    <!-- Save Button -->
+    <button @click="saveData">Save</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import VueDraggable from 'vue-draggable-plus';  // Import draggable component
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueDraggable
+  },
+  data() {
+    return {
+      blocks: [{ type: 'text', text: 'This is a draggable text block' }]  // Initial block data
+    };
+  },
+  methods: {
+    removeBlock(index) {
+      this.blocks.splice(index, 1); // Removes the block from the array
+    },
+    saveData() {
+      console.log(JSON.stringify(this.blocks)); // Logs the blocks data in JSON format
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.block {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
+.block-text {
+  width: 100%;
+  height: 100px;
+}
+
+button {
+  margin-top: 10px;
 }
 </style>
